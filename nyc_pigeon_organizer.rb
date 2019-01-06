59de3e248860b615dc8e61c6d1826d63d11fa9af
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 16:18:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |data_type, data|
     data.each do |quality, names|
       names.each do |name|
-        pigeon_list[name][data_type] = [] if pigeon_list[name][data_type] = nil
+        pigeon_list[name][data_type] = [""] if pigeon_list[name][data_type] = nil
         pigeon_list[name][data_type] << quality
       end
     end
